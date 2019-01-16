edb16e841433d865582535cbf7fc1390a2f6ad14
Scott Davis <scott@thirstyhead.com>
2019-01-16 16:07:34 -0700

    extracted css, js

---
+++ js/index.js
@@ -0,0 +1,29 @@
+window.addEventListener('load', init);
+
+function init(){
+  addFilter(document.querySelector('input[name="item-filter"]:checked').value);
+  enableItemFilter();  
+}
+
+function enableItemFilter(){
+  const itemFilters = document.querySelectorAll('input[name="item-filter"]');
+  for(let i=0; i<itemFilters.length; i++){
+    itemFilters[i].addEventListener('click', handleFilterEvent);
+  }
+}
+
+function handleFilterEvent(event){
+  addFilter(event.srcElement.value);        
+}
+
+function addFilter(filter){
+  const allItems = document.querySelectorAll('.item');
+  for(let i=0; i<allItems.length; i++){
+    let item = allItems[i];
+    if(item.dataset.category === filter){
+      item.classList.remove('hidden');
+    }else{
+      item.classList.add('hidden');
+    }
+  }
+}
