c96cbf865a6778e9e6747f4971428119b4599a0b
Scott Davis <scott@thirstyhead.com>
2019-02-12 14:09:53 -0700

    clean parse5 installation

--- utils/build-parse5.js
+++ utils/build-parse5.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const parse5 = require('parse5');
 
+
 let filename = 'index.html';
 let htmlSrc = fs.readFileSync(filename, 'utf8');
 let document = parse5.parse(htmlSrc);
@@ -20,13 +21,9 @@ function querySelector(parentElement, searchString){
   let result = undefined;
   if(searchString.startsWith('.')){
     let className = searchString.replace('.', '');
-    console.log(className);
-    console.log(parentElement);
     result = parentElement.childNodes.find( element => {
       if(element.attrs){
-        console.log(element.attrs);
         return element.attrs.find( attr => {
-          console.log(attr);
           return attr['name'] === 'class' && attr['value'] === className;
         });
       }
