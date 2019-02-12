cc4772af65f67ebf8d0f2e0ab1d9461fe0f81c2b
Scott Davis <scott@thirstyhead.com>
2019-02-12 11:48:43 -0700

    refactoring build.js to use home-grown querySelector

--- utils/build.js
+++ utils/build.js
@@ -4,17 +4,22 @@ const parse5 = require('parse5');
 let filename = 'index.html';
 let htmlSrc = fs.readFileSync(filename, 'utf8');
 let document = parse5.parse(htmlSrc);
-let html = document.childNodes.find( element => element.tagName === 'html' );
-let body = html.childNodes.find( element => element.tagName === 'body' );
+let html = querySelector(document, 'html'); 
+let body = querySelector(html, 'body'); 
 let container = body.childNodes.find( element => {
   if(element.attrs){
     return element.attrs.find( attr => attr.value === 'container');
   }
 });
-let main = container.childNodes.find( element => element.tagName === 'main' );
+let main = querySelector(container, 'main'); 
 main.childNodes = [];
 
 fs.writeFile(filename, parse5.serialize(document), err => {
   if(err) throw err;
   console.log(`Saved ${filename}`);
 });
+
+
+function querySelector(parentElement, searchString){
+  return parentElement.childNodes.find( element => element.tagName === searchString );
+}
