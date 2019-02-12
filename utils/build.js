6929602d55a6e814d0d49aaf82101c7a9debd4b3
Scott Davis <scott@thirstyhead.com>
2019-02-11 22:00:10 -0700

    node run build now empties out main element

--- utils/build.js
+++ utils/build.js
@@ -2,12 +2,17 @@ const fs = require('fs');
 const parse5 = require('parse5');
 
 let filename = 'index.html';
-let html = fs.readFileSync(filename, 'utf8');
-let document = parse5.parse(html);
-// console.log(document);
-
-// let main = JSON.stringify(document.childNodes, null, 2);
-// console.log(main);
+let htmlSrc = fs.readFileSync(filename, 'utf8');
+let document = parse5.parse(htmlSrc);
+let html = document.childNodes.find( element => element.tagName === 'html' );
+let body = html.childNodes.find( element => element.tagName === 'body' );
+let container = body.childNodes.find( element => {
+  if(element.attrs){
+    return element.attrs.find( attr => attr.value === 'container');
+  }
+});
+let main = container.childNodes.find( element => element.tagName === 'main' );
+main.childNodes = [];
 
 fs.writeFile(filename, parse5.serialize(document), err => {
   if(err) throw err;
